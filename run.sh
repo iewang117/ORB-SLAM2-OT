9bf5c63a2b80e4b24cbd4dd7675e01200f978193
iewang117 <iewang117@outlook.com>
2021-03-05 15:46:07 +0800

    first commit

--- run.sh
+++
@@ -1,1 +0,0 @@
-./Examples/RGB-D/rgbd_tum Vocabulary/ORBvoc.txt Examples/RGB-D/TUM3.yaml /home/qwe/dataset/rgbd_dataset_freiburg3_walking_xyz /home/qwe/dataset/rgbd_dataset_freiburg3_walking_xyz/association.txt
